779470108b57a246e3ef24a9a09ba0806552689c
XD <aledmikuin.it@gmail.com>
2026-01-29 16:53:25 +0100

    refactor: change in a pointer chasing obj with a better uaf demo

--- main.c
+++ main.c
@@ -1,40 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-typedef struct _Obj {   // struct Obj that handle a name & callback (for UAF & feng shui)
-    char name[32];
-    void (*callback)(); // here is what is interesting
+typedef struct _Obj {
+    void *current;
+    void * next;
+    void (*callback)();
 } Obj;
 
-void legit() {      // random normal func for testing
-    puts("legit callback");
+void legit() {
+    printf("Hello\n");
 }
 
+void uaf() {
+    printf("UAF\n");
+}
 
 int main() {
 
-    Obj *o = malloc(sizeof(Obj));   // create the 1st obj on the heap
-    strcpy(o->name, "Hello");   // random writing on o name
-    o->callback = legit;        // legit function on o callback
-    printf("Initial Addr: %p\n", o->callback); 
+    int size = 10;
 
-    Obj *spray[8096];   // create 8096 obj for spraying that can handle caching, segmentation heap, etc.
-    for (int i = 0; i < 8096; i++) {
+    Obj *spray[10];
+
+    for(int i = 0; i < size; i++) {
         spray[i] = malloc(sizeof(Obj));
+        spray[i]->current = NULL;
+        spray[i]->next = NULL;
+        spray[i]->callback = legit;
     }
 
-    free(o);    // free the initial obj for having a open space
+    for(int i = 0; i < size-1; i++) {
+        spray[i]->next = spray[i+1];
+        spray[i]->current = spray[i];
+    }
+    spray[size-1]->next = NULL; // breaks the loop by null the last ptr
 
-    for (int i = 0; i < 8096; i++) {    // we free them for no memeory leaks & clean heap (for reuse)
+
+    // free the spray we dont NULL the ptrs for UAF here
+    for(int i = 0; i < size; i++) {
         free(spray[i]);
     }
 
-    Obj *p = malloc(sizeof(Obj));               // alloc new obj for UAF
-    p->callback = (void*)0x4141414141414141;    // force overwrite on callback address, can replace by real function or w.e
-    printf("After Free Addr: %p\n", o->callback);
-    printf("After Free Name: %s\n", o->name);
-    o->callback();  // call the function from the address, can be dangerous // its here that the real exploit it
+    
+    Obj *evil = malloc(sizeof(Obj)); // create evil obj for uaf
+    evil->callback = uaf;           // we put the uaf func on the callback on the evil obj, in hope that evil obj will be in a spray space
+    evil->next = NULL;              // nullify the next to stop pointer chasing so we have a control
+
+    Obj *p = spray[0];
+    // Loop on the spray for info
+    while(p) {
+        printf("Current: %p | Next: %p | Callback: %p\n", p->current, p->next, p->callback);
+        p->callback();  // we call the callback func on spray object, but i MAY have been UAF by the evil
+        p = p->next;
+    }
 
     return 0;
+    
 }
